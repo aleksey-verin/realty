8d493cacf42bc80ffc494db12a0195515d52e144
Aleksey Verin <verevaa@yandex.ru>
2023-04-27 23:25:32 +0300

    fix: cleaned the import for deploy

--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef, useState } from 'react';
+import { FC } from 'react';
 import { routesType } from '../routes/routes';
 import { NavLink } from 'react-router-dom';
 
@@ -7,32 +7,11 @@ interface SidebarItemProps {
 }
 
 const SidebarItem: FC<SidebarItemProps> = ({ data }) => {
-  const { path, title, menuImage } = data; // will add 'path'
-  // const [isActive, setIsActive] = useState(false);
-  // const [over, setOver] = useState(false);
-
-  // const myDiv = useRef<HTMLAnchorElement>(null);
-
-  // useEffect(() => {
-  //   if (!myDiv.current) return;
-  //   if (myDiv.current.classList.contains('active')) {
-  //     setIsActive(true);
-  //   } else {
-  //     setIsActive(false);
-  //   }
-  //   console.log(myDiv.current.classList.contains('sidebar-item')); // change to 'active'
-  // }, [myDiv]);
+  const { path, title, menuImage } = data;
 
   return (
-    <NavLink
-      to={path}
-      // ref={myDiv}
-      // onMouseOver={() => setOver(true)}
-      // onMouseOut={() => setOver(false)}
-      className="sidebar-item"
-      title={title}>
+    <NavLink to={path} className="sidebar-item" title={title}>
       {({ isActive }) => (isActive ? menuImage.active : menuImage.normal)}
-      {/* {isActive ? images.active : images.normal} */}
     </NavLink>
   );
 };
