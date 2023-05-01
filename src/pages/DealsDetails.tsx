8593480158402cd2f61d7141981dc7289839b095
Aleksey Verin <verevaa@yandex.ru>
2023-05-01 19:44:25 +0300

    feat: finished work in the layout on Deals Details

--- src/pages/DealsDetails.tsx
+++ src/pages/DealsDetails.tsx
@@ -1,18 +1,113 @@
 import { FC } from 'react';
 // import { useParams } from 'react-router-dom';
 import Header from '../components/Header';
+import ImgEdit from '../components/ui/images/ImgEdit';
+import ImgDelete from '../components/ui/images/ImgDelete';
+import ImgNotfound from '../components/ui/images/ImgNotfound';
 
 interface DealsDetailsProps {}
 
 const DealsDetails: FC<DealsDetailsProps> = () => {
   // const { id } = useParams();
 
+  const imageCustomer = {
+    backgroundImage: `url()`
+  };
+
   return (
     <div className="content">
       <Header headerTitle="Deal Details" isButtonVisible={false} isAsideBlockVisible={true} />
       <main>
         <div className="details">
-          <div className="details-main">details</div>
+          <div className="details-main">
+            <div className="details-main__info information">
+              <div className="information-item">
+                <div
+                  style={imageCustomer}
+                  className="information-item__image column-picture__pic"></div>
+                <div className="information-item__info">
+                  <div>Customer</div>
+                  <div>David Piotrovsky</div>
+                </div>
+              </div>
+              <div className="information-item">
+                <div className="information-item__info">
+                  <div>Email</div>
+                  <div>brodrigues@gmail.com</div>
+                </div>
+              </div>
+              <div className="information-item">
+                <div className="information-item__info">
+                  <div>Phone</div>
+                  <div>617 962 4563</div>
+                </div>
+              </div>
+            </div>
+            <div className="details-main__middle middle">
+              <div className="middle-text">
+                2893 Austin Secret Lane, Parowan, UT 12413 Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Soluta, porro.
+              </div>
+              <div className="middle-delete">
+                <ImgDelete />
+              </div>
+              <div className="middle-edit">
+                <ImgEdit />
+              </div>
+            </div>
+            <div className="details-main__basic basic">
+              <div className="basic-info">
+                <div className="information-item">
+                  <div className="information-item__info">
+                    <div>Progress</div>
+                    <div>In Progress</div>
+                  </div>
+                </div>
+                <div className="information-item">
+                  <div className="information-item__info">
+                    <div>Appointment Date</div>
+                    <div>Nov 17, 2021 08:00</div>
+                  </div>
+                </div>
+                <div className="information-item">
+                  <div className="information-item__info">
+                    <div>Room Area</div>
+                    <div>25m2</div>
+                  </div>
+                </div>
+                <div className="information-item">
+                  <div className="information-item__info">
+                    <div>Number of people</div>
+                    <div>10</div>
+                  </div>
+                </div>
+                <div className="information-item">
+                  <div className="information-item__info">
+                    <div>Price</div>
+                    <div>$ 6000</div>
+                  </div>
+                </div>
+                <div className="information-item">
+                  <div className="information-item__info">
+                    <div>Room Access</div>
+                    <div>Keys with doorman</div>
+                  </div>
+                </div>
+                <div className="information-item">
+                  <div className="information-item__info">
+                    <div>Special Instructions</div>
+                    <div>
+                      At risus viverra adipiscing at in tellus. Blandit massa enim nec dui nunc
+                      mattis. Lacus vel facilisis volutpat est velit.
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="basic-image">
+                <div className="basic-image__pic"></div>
+              </div>
+            </div>
+          </div>
           <div className="details-aside">
             <div className="details-aside__record record">
               <div className="record-title">Record Activity</div>
@@ -50,6 +145,12 @@ const DealsDetails: FC<DealsDetailsProps> = () => {
                     <div className="log-content__date">17 Apr 2023</div>
                     <div className="log-content__text">Installation or inspection</div>
                   </div>
+                </div>
+                <div className="log-items__empty">
+                  <div>
+                    <ImgNotfound />
+                  </div>
+                  <div>No activity found.</div>
                 </div>
               </div>
               <div className="log-loader">
