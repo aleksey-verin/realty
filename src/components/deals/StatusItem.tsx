e3b13f5b98c401a79cdab0fbce9665d06b3a7875
Aleksey Verin <verevaa@yandex.ru>
2023-05-10 17:11:38 +0300

    fix: some bugs

--- src/components/deals/StatusItem.tsx
+++ src/components/deals/StatusItem.tsx
@@ -1,18 +1,20 @@
 import { FC } from 'react';
 import { contentLocal } from '../../utils/constants/contentLocal';
 import { progressStatusForDeals } from '../../utils/constants/constants';
+import { useSelector } from 'react-redux';
+import { selectorLocalization } from '../../store/reducers/localizationSlice';
 
 interface StatusItemProps {
   status: string;
 }
 
 const StatusItem: FC<StatusItemProps> = ({ status }) => {
-  const local = 'rus';
+  const { lang } = useSelector(selectorLocalization);
 
   const viewedProgress =
     status === progressStatusForDeals.inProgress
-      ? contentLocal.pages.deals.progressStatuses.inProgress[local]
-      : contentLocal.pages.deals.progressStatuses.closed[local];
+      ? contentLocal.pages.deals.progressStatuses.inProgress[lang]
+      : contentLocal.pages.deals.progressStatuses.closed[lang];
 
   return (
     <div className={`status ${status === progressStatusForDeals.closed ? 'closed' : ''}`}>
